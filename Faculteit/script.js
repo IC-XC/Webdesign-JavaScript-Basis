04f39e4dad9bf6789705eeb9bc5eb6f48ef74f31
Per <86513555+IC-XC@users.noreply.github.com>
2023-03-18 20:41:02 +0100

    Add files via upload

---
+++ Faculteit/script.js
@@ -0,0 +1,18 @@
+function fact(){
+    var i, num, f;
+    f = 1;
+    num = document.getElementById("getal").value;
+    for(i = 1; i <= num; i++)  
+    {
+    f = f * i;
+    }
+    i = i - 1;  
+
+    if (num < 0) {
+        alert("De faculteit van een negatief getal bestaat niet")
+    }
+    document.getElementById("output").innerHTML = "De faculteit van het getal " + i + " is: " + f ;
+
+   
+
+    }
